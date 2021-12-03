0f18c6b531606f26a437eee96bfa0c74dbbb4be0
Madh93 <mhdez@protonmail.com>
2021-12-03 20:21:24 +0000

    ci: use reproducible builds

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,11 +4,17 @@ LAMBDA_NAME=$1
 
 build_and_zip() {
     echo "Packaging '$1' lambda..."
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build lambda/$1/main.go && touch --date=@0 main && zip -rq artifacts/$1.zip main && rm main
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build lambda/$1/main.go && .bin/deterministic-zip -q artifacts/$1.zip main && rm main
 }
 
-mkdir -p artifacts
+# Install dependencies
+mkdir -p artifacts .bin
+if [ ! -f ".bin/deterministic-zip" ]; then
+  curl -o .bin/deterministic-zip -sSLO https://github.com/timo-reymann/deterministic-zip/releases/download/$(curl -Lso /dev/null -w %{url_effective} https://github.com/timo-reymann/deterministic-zip/releases/latest | grep -o '[^/]*$')/deterministic-zip_linux-amd64
+  chmod +x .bin/deterministic-zip
+fi
 
+# Package
 if [[ -z "$LAMBDA_NAME" ]]; then
     for d in `ls -1 lambda`; do
         build_and_zip $d
