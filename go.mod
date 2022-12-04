d4e45df757464c5b9444c5c04f9baff8d40972a1
Madh93 <mhdez@protonmail.com>
2022-12-04 01:49:48 +0000

    feat: use Telegram Bot API library

--- go.mod
+++ go.mod
@@ -4,26 +4,34 @@ go 1.19
 
 require (
 	github.com/aws/aws-lambda-go v1.34.1
-	github.com/aws/aws-sdk-go-v2 v1.16.11
+	github.com/aws/aws-sdk-go v1.44.152
+	github.com/aws/aws-sdk-go-v2 v1.17.2
 	github.com/aws/aws-sdk-go-v2/config v1.17.1
 	github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue v1.9.12
 	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.16.0
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.29.5
 	github.com/aws/aws-sdk-go-v2/service/sns v1.17.13
 	github.com/aws/aws-sdk-go-v2/service/ssm v1.27.9
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/technoweenie/multipartstreamer v1.0.1
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.10 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.12.14 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.12 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.18 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.12 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.26 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.20 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.19 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.17 // indirect
 	github.com/aws/aws-sdk-go-v2/service/dynamodbstreams v1.13.14 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.11 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.21 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.7.12 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.12 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.20 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.20 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.11.17 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.16.13 // indirect
-	github.com/aws/smithy-go v1.12.1 // indirect
+	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 )
--- lambda/send_telegram_notification/main.go
+++ lambda/send_telegram_notification/main.go
@@ -1,37 +1,25 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 var (
-	telegramChatID = os.Getenv("TELEGRAM_CHAT_ID")
+	telegramChatID, _ = strconv.ParseInt(os.Getenv("TELEGRAM_CHAT_ID"), 10, 64)
+	telegramChatName  = os.Getenv("TELEGRAM_CHAT_NAME")
 
 	client *ssm.Client
 )
-
-type sendDocumentReqBody struct {
-	ChatID   string `json:"chat_id"`
-	Document string `json:"document"`
-}
-
-type telegramResponse struct {
-	Ok          bool   `json:"ok"`
-	ErrorCode   int    `json:"error_code,omitempty"`
-	Description string `json:"description,omitempty"`
-}
 
 func exitErrorf(msg string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, msg+"\n", args...)
@@ -39,7 +27,7 @@ func exitErrorf(msg string, args ...interface{}) {
 }
 
 func sendTelegramNotification(ctx context.Context, document string) error {
-	fmt.Printf("Sending new announcement to %q Telegram channel...\n", telegramChatID)
+	fmt.Printf("Sending new announcement to %q Telegram channel...\n", telegramChatName)
 
 	// Retrieve Telegram Auth Token
 	result, err := client.GetParameter(ctx, &ssm.GetParameterInput{
@@ -50,40 +38,20 @@ func sendTelegramNotification(ctx context.Context, document string) error {
 		return fmt.Errorf("unable to get Telegram Auth Token from SSM, %w", err)
 	}
 
-	// Build Telegram request
-	url := fmt.Sprintf("https://api.telegram.org/bot%s/sendDocument", *result.Parameter.Value)
-	reqBody := &sendDocumentReqBody{
-		ChatID:   telegramChatID,
-		Document: document,
-	}
-
-	reqBytes, err := json.Marshal(reqBody)
+	// Initialize bot
+	bot, err := tgbotapi.NewBotAPI(*result.Parameter.Value)
 	if err != nil {
-		return fmt.Errorf("unable to marshal request body json, %w", err)
+		return fmt.Errorf("unable to initialize Telegram Bot, %w", err)
 	}
 
 	// Send notification
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(reqBytes))
+	msg := tgbotapi.NewDocument(telegramChatID, tgbotapi.FilePath(document))
+	_, err = bot.Send(msg)
 	if err != nil {
-		return fmt.Errorf("unable to post %q url, %w", url, err)
-	}
-	defer resp.Body.Close()
-
-	// Parse response
-	var response telegramResponse
-	respBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("unable to read response body, %w", err)
-	}
-	if err := json.Unmarshal(respBytes, &response); err != nil {
-		return fmt.Errorf("unable to unmarshal response body json, %w", err)
+		return fmt.Errorf("unable to send document %w", err)
 	}
 
-	if !response.Ok {
-		return fmt.Errorf("unexpected %d status code! %v", response.ErrorCode, response.Description)
-	}
-
-	fmt.Printf("The announcement %q was sent to %q Telegram channel successfully!\n", document, telegramChatID)
+	fmt.Printf("The announcement %q was sent to %q Telegram channel successfully!\n", document, telegramChatName)
 	return nil
 }
 
