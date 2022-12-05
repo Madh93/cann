69e6591020918032ca47cb8874cb1e93e488d53b
Madh93 <mhdez@protonmail.com>
2022-12-05 00:12:19 +0000

    build: parallelize builds with gox

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,26 +1,40 @@
 #! /bin/bash
 
 LAMBDA_NAME=$1
+OUTPUT_DIR=artifacts
+GO_MODULE_NAME="gitlab.com/Madh93/cann"
+GO_OS_ARCH="linux/amd64"
 
-build_and_zip() {
-    echo "Packaging '$1' lambda..."
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build lambda/$1/main.go && .bin/deterministic-zip -q artifacts/$1.zip main && rm main
+build() {
+  echo "Building..."
+  if [ -z "$1" ]; then
+    target="./..."
+  else
+    target="$GO_MODULE_NAME/lambda/$1"
+  fi
+  mkdir -p $OUTPUT_DIR
+  $GOPATH/bin/gox -osarch="$GO_OS_ARCH" -output="$OUTPUT_DIR/{{.Dir}}" $target
+  echo "Building... Done!"
+}
+
+zip() {
+  echo -n "Zipping '$1' lambda..."
+  cp $OUTPUT_DIR/$1 main && $GOPATH/bin/deterministic-zip -q $OUTPUT_DIR/$1.zip main && rm main
+  echo " Done!"
 }
 
 # Install dependencies
-mkdir -p artifacts .bin
-if [ ! -f ".bin/deterministic-zip" ]; then
-  curl -o .bin/deterministic-zip -sSLO https://github.com/timo-reymann/deterministic-zip/releases/download/$(curl -Lso /dev/null -w %{url_effective} https://github.com/timo-reymann/deterministic-zip/releases/latest | grep -o '[^/]*$')/deterministic-zip_linux-amd64
-  chmod +x .bin/deterministic-zip
-fi
+go install github.com/mitchellh/gox@v1.0.1
+go install github.com/timo-reymann/deterministic-zip@1.2.0
 
 # Package
 if [[ -z "$LAMBDA_NAME" ]]; then
-    for d in `ls -1 lambda`; do
-        build_and_zip $d
-    done
+  build
+  for d in `ls -1 lambda`; do
+    zip $d
+  done
 else
-    build_and_zip $LAMBDA_NAME
+  build $LAMBDA_NAME && zip $LAMBDA_NAME
 fi
 
-echo "Done! The packages are available under 'artifacts' directory."
+echo "Done! The packages are available under '$OUTPUT_DIR' directory."
